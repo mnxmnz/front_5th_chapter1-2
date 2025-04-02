67e827b3dcdcbdf49584876f07710c6f249afa4c
mnxmnz <minzidev@gmail.com>
2025-04-02 23:13:47 +0900

    feat: createVNode 함수에 vNode 객체 생성 로직 추가 및 자식 요소 평탄화 및 유효성 검사 기능 구현

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,15 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  // type, props, children 속성을 가진 vNode(Virtual Node) 객체 생성
+  return {
+    type,
+    props,
+    children: children
+      // 모든 중첩 배열을 1차원 배열로 평탄화
+      // 평탄화를 하는 이유: JSX 구문에서 자식 요소가 중첩된 형태로 들어올 수 있기 때문
+      .flat(Infinity)
+      // 유효하지 않은 값 제거
+      .filter(
+        (child) => child !== false && child !== null && child !== undefined,
+      ),
+  };
 }
