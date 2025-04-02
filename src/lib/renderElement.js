0748d94359eb0dcb761ba1fa1da50607ef223e11
mnxmnz <minzidev@gmail.com>
2025-04-03 00:00:24 +0900

    feat: renderElement 함수에서 DOM 업데이트 로직 개선 및 이벤트 위임 방식으로 리스너 등록

--- src/lib/renderElement.js
+++ src/lib/renderElement.js
@@ -1,10 +1,16 @@
 import { setupEventListeners } from "./eventManager";
 import { createElement } from "./createElement";
 import { normalizeVNode } from "./normalizeVNode";
-import { updateElement } from "./updateElement";
+// import { updateElement } from "./updateElement";
 
 export function renderElement(vNode, container) {
-  // 최초 렌더링시에는 createElement로 DOM을 생성하고
-  // 이후에는 updateElement로 기존 DOM을 업데이트한다.
-  // 렌더링이 완료되면 container에 이벤트를 등록한다.
+  const normalizedVNode = normalizeVNode(vNode);
+  const $el = createElement(normalizedVNode);
+
+  // 기존 내용을 지우고 새로운 내용으로 교체
+  container.innerHTML = "";
+  container.appendChild($el);
+
+  // 이벤트 위임을 사용하여 최상위 컨테이너에 하나의 리스너만 등록
+  setupEventListeners(container);
 }
